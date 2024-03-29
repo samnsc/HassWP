f25c2214aa93da8567e91a0801912ec3817c375a
Samuel Nascimento <professional@samnsc.com>
2024-03-29 12:45:07 -0300

    Update setup.py to latest version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name="hass_win",
-    version="2023.12.4",
+    version="2024.3.3",
     description="Windows runner for Home Assistant",
     author="AlexxIT",
     install_requires=[
         "homeassistant",
         "colorlog",  # optional color log
-        "chacha20poly1305-reuseable==0.0.4",  # fix HomeKit support on after 2023.5
+        "chacha20poly1305-reuseable",  # fix HomeKit support on after 2023.5
     ],
     packages=["hass_win"],
     package_data={"hass_win": ["*.dll"]},
